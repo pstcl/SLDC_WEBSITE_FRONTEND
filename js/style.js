2956a56f9661e68a25bb2318050bfd2d58cd2205
pstcl <40257916+pstcl@users.noreply.github.com>
2019-09-09 14:57:25 +0530

    asdf

--- js/style.js
+++ js/style.js
@@ -1,51 +1,51 @@
 function changeTheme(attr) {
-  var href;
-  switch (attr) {
-    case "B":
-      href = "./css/colorBlue.css?v=6";
-      setCookie("themeColor", attr, 100);
-      break;
-    case "R":
-      href = "./css/colorRed.css?v=6";
-      setCookie("themeColor", attr, 100);
-      break;
-    case "Y":
-      href = "./css/colorYellow.css?v=6";
-      setCookie("themeColor", attr, 100);
-      break;
+    var href;
+    switch (attr) {
+        case "B":
+            href = "./css/colorBlue.css?v=7";
+            setCookie("themeColor", attr, 100);
+            break;
+        case "R":
+            href = "./css/colorRed.css?v=7";
+            setCookie("themeColor", attr, 100);
+            break;
+        case "Y":
+            href = "./css/colorYellow.css?v=7";
+            setCookie("themeColor", attr, 100);
+            break;
 
-    default:
-      href = "./css/colorRed.css?v=6";
-      setCookie("themeColor", "B", 100);
+        default:
+            href = "./css/colorRed.css?v=7";
+            setCookie("themeColor", "B", 100);
 
-      break;
-  }
-  document.getElementById("styleSheetColor").href = href;
+            break;
+    }
+    document.getElementById("styleSheetColor").href = href;
 }
 
 function defaultTheme() {
-  changeTheme(getCookie("themeColor"));
+    changeTheme(getCookie("themeColor"));
 }
 
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  var expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
-  var name = cname + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(";");
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
